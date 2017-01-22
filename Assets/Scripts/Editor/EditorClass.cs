c57b651b1c4eafc10e1fe9ed94447e8489a84174
KonH <konh@yandex.ru>
2017-01-22 12:19:48 +0600

    Editor class example;

---
+++ Assets/Scripts/Editor/EditorClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestNamespace.Editor {
+	/// <summary>
+	/// Editor class summary
+	/// </summary>
+	public class EditorClass : MonoBehaviour {
+	}
+}
